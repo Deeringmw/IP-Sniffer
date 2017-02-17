7a55c8924f13f7397a141837d35f1b9d1657661c
Marshall <deeringmw@gmail.com>
2017-02-17 09:50:48 -0500

    Basic IP info retrieval tool

---
+++ IP Sniffer/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace IP_Sniffer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        public void txtIP_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtIP.Text = "";
+        }
+        private void btnSniff_Click(object sender, RoutedEventArgs e)
+        {
+          
+            using (WebClient wc = new WebClient())
+            {
+                var ip = txtIP.Text;
+                var json = wc.DownloadString("http://ipinfo.io/" + ip + "/json");
+
+                var ipData = JsonConvert.DeserializeObject<IP>(json);
+
+                lstData.Visibility = Visibility.Visible;
+                var lstDataItems = new List<string>();
+                lstData.Items.Add(ipData.ip);
+                lstData.Items.Add(ipData.hostname);
+                lstData.Items.Add(ipData.city);
+                lstData.Items.Add(ipData.region);
+                lstData.Items.Add(ipData.country);
+                lstData.Items.Add(ipData.loc);
+                lstData.Items.Add(ipData.org);
+                lstData.Items.Add(ipData.postal);
+            }
+
+        }
+    }
+}
---
+++ IP Sniffer/ipClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IP_Sniffer
+{
+    public class IP
+    {
+        public string ip { get; set; }
+        public string hostname { get; set; }
+        public string city { get; set; }
+        public string region { get; set; }
+        public string country { get; set; }
+        public string loc { get; set; }
+        public string org { get; set; }
+        public string postal { get; set; }
+    }
+}
